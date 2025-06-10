a8d5fae3046ae8700a294f48c35157caaf0a0e02
shreyansh0587 <appwayjobs@gmail.com>
2025-06-10 22:24:37 +0530

    code commited on 10 june 2025 2nd time

--- src/Table.java
+++ src/Table.java
@@ -1,12 +1,48 @@
 public class Table {
     public static void main(String[] args) {
         int num=9;
+        int a=9;
 
-        for (int i = 1; i <= 10; i++) {
+      /*  for (int i = 1; i <= 10; i++) {
             System.out.println("---------");
             System.out.printf("%d * %2d = %d%n", num, i, num * i);
             //System.out.printf("%d\t%d\t\n",a,b);
+        }*/
+
+      int b = 1;
+                System.out.printf("%d * %d=%d", a, b, a * b);
+                System.out.println();
+                b++;
+
+                System.out.printf("%d * %d=%d", a, b, a * b);
+                System.out.println();
+                b++;
+                System.out.printf("%d * %d=%d", a, b, a * b);
+                System.out.println();
+                b++;
+                System.out.printf("%d * %d=%d", a, b, a * b);
+                System.out.println();
+                b++;
+                System.out.printf("%d * %d=%d", a, b, a * b);
+                System.out.println();
+                b++;
+                System.out.printf("%d * %d=%d", a, b, a * b);
+                System.out.println();
+                b++;
+                System.out.printf("%d * %d=%d", a, b, a * b);
+                System.out.println();
+                b++;
+                System.out.printf("%d * %d=%d", a, b, a * b);
+                System.out.println();
+                b++;
+                System.out.printf("%d * %d=%d", a, b, a * b);
+                System.out.println();
+                b++;
+                System.out.printf("%d * %d=%d", a, b, a * b);
+                System.out.println();
+                b++;
+            }
         }
 
-    }
-}
+
+
